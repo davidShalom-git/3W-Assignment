7bded005ff5a3c0eee81faaedd98af8719b20596
davidshalom-git <davidshalom2609@gmail.com>
2026-09-16 18:31:38 +0530

    Remove frontend and backend, keep pure room showcase static site

--- frontend/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import client from './client';
-
-export function signup({ username, email, password }) {
-  return client.post('/api/auth/signup', { username, email, password }).then((res) => res.data);
-}
-
-export function login({ email, password }) {
-  return client.post('/api/auth/login', { email, password }).then((res) => res.data);
-}
